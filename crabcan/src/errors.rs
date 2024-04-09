8217a67def49536fd86c79d6dff46f657ff3761c
Luis David Garcia <hookeri_bartz@aleeas.com>
2024-04-08 20:22:30 -0700

    Add nix and scan_fmt dependencies to crabcan/Cargo.toml

--- crabcan/src/errors.rs
+++ crabcan/src/errors.rs
@@ -1,6 +1,7 @@
 use std::fmt;
 use std::process::exit;
 
+use crate::container::MINIMAL_KERNEL_VERSION;
 // Allows to display a variant with the format {:?}
 #[derive(Debug)]
 // Containers all possible errors in our tool
@@ -26,6 +27,18 @@ impl fmt::Display for Errcode {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         // Define what behavior to have for each variant
         match &self {
+
+            Errcode::NotSupported(errtype) => {
+                match errtype {
+                    0 => write!(f, "Minimal kernel version required: {}", MINIMAL_KERNEL_VERSION),
+                    1 => write!(f, "Only x86_64 architecture is supported"),
+                    _ => write!(f, "{:?} (unknown id)", self),
+                }
+            }
+
+            // Message to display when an argument is invalid
+            Errcode::ArgumentInvalid(element) => write!(f, "ArgumentInvalid: {}", element),
+
             _ => write!(f, "{:?}", self)
         }
     }
@@ -39,7 +52,7 @@ pub fn exit_with_retcode(res: Result<(), Errcode>) {
             log::debug!("Exit without any error, returning 0");
             exit(0);
         }, 
-        
+
         // If there's an error, print an error message and return the corresponding error code
         Err(e) =>  {
             let retcode = e.get_retcode();
