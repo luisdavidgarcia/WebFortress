3fb5024fb58c43a521f5d18c2979cd22b41e5d88
Luis David Garcia <hookeri_bartz@aleeas.com>
2024-04-09 08:06:27 -0700

    Add syscalls module and implement setsyscalls function

--- crabcan/src/errors.rs
+++ crabcan/src/errors.rs
@@ -1,60 +1,72 @@
-use crate::errors::Errcode;
-use crate::config::ContainerOpts;
-use crate::namespaces::userns;
-use crate::hostname::set_container_hostname;
-use crate::mounts::setmountpoint;
-use crate::capabilities::setcapabilities;
+use std::fmt;
+use std::process::exit;
 
-use nix::unistd::{Pid, close};
-use nix::sched::clone;
-use nix::sys::signal::Signal;
-use nix::sched::CloneFlags;
-
-const STACK_SIZE: usize = 1024 * 1024;
-fn setup_container_configurations(config: &ContainerOpts) -> Result<(), Errcode> {
-    set_container_hostname(&config.hostname)?;
-    setmountpoint(&config.mount_dir)?;
-    userns(config.fd, config.uid)?;
-    setcapabilities()?;
-    Ok(())
+use crate::container::MINIMAL_KERNEL_VERSION;
+// Allows to display a variant with the format {:?}
+#[derive(Debug)]
+// Contains all possible errors in our tool
+pub enum Errcode{
+    ArgumentInvalid(&'static str),
+    NotSupported(u8),
+    SocketError(u8),
+    ContainerError(u8),
+    ChildProcessError(u8),
+    MountsError(u8),
+    NamespacesError(u8),
+    HostnameError(u8),
+    CapabilitiesError(u8),
+    SyscallsError(u8),
+    RngError
 }
 
-fn child(config: ContainerOpts) -> isize {
-    match setup_container_configurations(&config) {
-        Ok(_) => log::info!("Container set up successfully"),
-        Err(e) => {
-            log::error!("Error while configuring container: {:?}", e);
-            return -1;
+impl Errcode{
+    // Translate an Errcode::X into a number to return (the Unix way)
+    pub fn get_retcode(&self) -> i32 {
+        1 // Everything != 0 will be treated as an error
+    }
+}
+
+
+#[allow(unreachable_patterns)]
+// trait Display, allows Errcode enum to be displayed by:
+//      println!("{}", error);
+//  in this case, it calls the function "fmt", which we define the behaviour below
+impl fmt::Display for Errcode {
+
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // Define what behaviour for each variant of the enum
+        match &self{
+
+            Errcode::NotSupported(errtype) => {
+                match errtype {
+                    0 => write!(f, "Minimal kernel version required: {}", MINIMAL_KERNEL_VERSION),
+                    1 => write!(f, "Only x86_64 architecture is supported"),
+                    _ => write!(f, "{:?} (unknown id)", self),
+                }
+            },
+
+            // Message to display when an argument is invalid
+            Errcode::ArgumentInvalid(element) => write!(f, "ArgumentInvalid: {}", element),
+
+            _ => write!(f, "{:?}", self) // For any variant not previously covered
         }
     }
-
-    if let Err(_) = close(config.fd){
-        log::error!("Error while closing socket ...");
-        return -1;
-    }
-
-    log::info!("Starting container with command {} and args {:?}", config.path.to_str().unwrap(), config.argv);
-    0
 }
 
-pub fn generate_child_process(config: ContainerOpts) -> Result<Pid, Errcode> {
-    let mut tmp_stack: [u8; STACK_SIZE] = [0; STACK_SIZE];
-    let mut flags = CloneFlags::empty();
-    flags.insert(CloneFlags::CLONE_NEWNS);
-    flags.insert(CloneFlags::CLONE_NEWCGROUP);
-    flags.insert(CloneFlags::CLONE_NEWPID);
-    flags.insert(CloneFlags::CLONE_NEWIPC);
-    flags.insert(CloneFlags::CLONE_NEWNET);
-    flags.insert(CloneFlags::CLONE_NEWUTS);
+// Get the result from a function, and exit the process with the correct error code
+pub fn exit_with_retcode(res: Result<(), Errcode>) {
+    match res {
+        // If it's a success, return 0
+        Ok(_) => {
+            log::debug!("Exit without any error, returning 0");
+            exit(0);
+        },
 
-    match clone(
-        Box::new(|| child(config.clone())),
-        &mut tmp_stack,
-        flags,
-        Some(Signal::SIGCHLD as i32)
-    )
-    {
-        Ok(pid) => Ok(pid),
-        Err(_) => Err(Errcode::ChildProcessError(0))
+        // If there's an error, print an error message and return the retcode
+        Err(e) => {
+            let retcode = e.get_retcode();
+            log::error!("Error on exit:\n\t{}\n\tReturning {}", e, retcode);
+            exit(retcode);
+        }
     }
 }
