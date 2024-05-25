3716bf458491e8f3cc6d561bee68951f79b29725
Luis David Garcia <hookeri_bartz@aleeas.com>
2024-05-25 15:40:02 -0700

    Resolved monster edits

--- setup.sh
+++ setup.sh
@@ -6,20 +6,7 @@ set -e
 # Function to install dependencies on Linux
 install_linux_dependencies() {
     echo "Updating and installing necessary packages for Linux..."
-    sudo apt-get update && sudo apt-get install -y curl git build-essential python3-pip docker.io libseccomp-dev
-    sudo usermod -aG docker $USER
-    echo "NOTE: You may need to log out and log back in for Docker group changes to take effect."
-}
-
-# Function to install dependencies on macOS
-install_mac_dependencies() {
-    echo "Updating and installing necessary packages for macOS..."
-    if ! command -v brew &>/dev/null; then
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-    fi
-    brew install curl git python3 docker
-    open /Applications/Docker.app
-    echo "Please follow any additional instructions to complete Docker installation."
+    sudo apt-get update && sudo apt-get install -y curl git build-essential python3-pip libseccomp-dev
 }
 
 # Function to install Rust
@@ -38,53 +25,15 @@ install_and_setup_pre_commit() {
 
 # Detect OS and install dependencies
 detect_and_install_dependencies() {
-    OS="`uname`"
-    case $OS in
-      'Linux')
-        install_linux_dependencies
-        ;;
-      'Darwin')
-        install_mac_dependencies
-        ;;
-      *)
-        echo "Operating system $OS not supported."
+    if [ "$(uname)" != "Linux" ]; then
+        echo "This script can only be run on a Linux environment."
         exit 1
-        ;;
-    esac
-
-    #install_rust
+    fi
+    install_linux_dependencies
     install_and_setup_pre_commit
 }
 
-# Docker image and container management
-manage_docker() {
-    IMAGE_NAME="rustosprojects"
-    CONTAINER_NAME="rustos_dev"
-
-    if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
-      echo "Building Docker image '$IMAGE_NAME'..."
-      docker build -t $IMAGE_NAME .
-    else
-      echo "Docker image '$IMAGE_NAME' already exists."
-    fi
-
-    if [ "$(docker ps -aq -f name=^${CONTAINER_NAME}$)" ]; then
-        if [ ! "$(docker ps -q -f name=^${CONTAINER_NAME}$)" ]; then
-            echo "Starting existing container '$CONTAINER_NAME'..."
-            docker start $CONTAINER_NAME
-        else
-            echo "Container '$CONTAINER_NAME' is already running."
-        fi
-    else
-        echo "Creating and starting new container '$CONTAINER_NAME'..."
-        docker run -dit --name $CONTAINER_NAME -v "$(pwd):/workspace" $IMAGE_NAME
-    fi
-
-    echo "Attaching to the container '$CONTAINER_NAME'. Press Ctrl+D to detach."
-    docker attach $CONTAINER_NAME
-}
-
-# Main function to orchestrate setup and docker management
+# Main function to orchestrate setup
 main() {
     detect_and_install_dependencies
 }
