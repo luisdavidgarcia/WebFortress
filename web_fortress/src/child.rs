291ef93e16bd789e53426dcbe9170194bdf64819
luisdavidgarcia <hookeri_bartz@aleeas.com>
2024-06-05 22:51:05 +0000

    cleaning of files

--- web_fortress/src/child.rs
+++ web_fortress/src/child.rs
@@ -51,16 +51,17 @@ fn child(config: ContainerOpts) -> isize {
     // let env_vars: Vec<CString> = vec![];
 
     // There are for juiceshop attempt
-    let env_vars: Vec<CString> = vec![
-        CString::new("LD_LIBRARY_PATH=/usr/lib:/usr/lib64:/usr/local/lib").unwrap(),
-        CString::new("HOME=/").unwrap(),
-    ];
+    // let env_vars: Vec<CString> = vec![
+    //     CString::new("LD_LIBRARY_PATH=/usr/lib:/usr/lib64:/usr/local/lib").unwrap(),
+    //     CString::new("HOME=/").unwrap(),
+    // ];
 
     // These  were in attempt for webgoat
-    // let env_vars: Vec<CString> = vec![
-    //     CString::new("LD_LIBRARY_PATH=/usr/lib/jvm/java-17-openjdk-amd64/lib").unwrap(),
-    //     CString::new("JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64").unwrap(),
-    // ];
+    let env_vars: Vec<CString> = vec![
+        CString::new("LD_LIBRARY_PATH=/usr/lib/jvm/java-17-openjdk-amd64/lib").unwrap(),
+        CString::new("JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64").unwrap(),
+        CString::new("HOME=/").unwrap(),
+    ];
 
     log::debug!("Environment variables: {:?}", env_vars);
 
